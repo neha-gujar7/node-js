db29393855b11ce8d59ef6ba5ec63d1598f2ad8c
Your Name <your.email@example.com>
2025-05-27 21:12:24 +0530

    initial commit

---
+++ helloworld/hello.js
@@ -0,0 +1,13 @@
+// console.log("hey there is JS");
+// //for make it run in terminal,we write usually node filename.js
+// //but here we can write node file-name only becuase node can only execute JS files
+
+// function add(a,b){
+//     return a+b;
+// }
+const math = require('./math');
+// const math=require(./math);
+console.log(math);
+// console.log("math value is:", math(4,8));
+
+
---
+++ helloworld/math.js
@@ -0,0 +1,11 @@
+// console.log(3,4);
+
+function add(a,b){
+    return a+b;
+}
+// module.exports=add; 
+// console.log(add(2,5));
+module.exports="piyush";
+
+// module.exports=add;  //so this line is showing/ returning a function
+
